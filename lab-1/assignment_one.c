c979052e4b1e89012a87a501987743c8c088ccbc
Simon Johannesson <simonljohannesson@gmail.com>
2020-08-28 14:53:06 +0200

    Just saving

--- lab-1/assignment_one.c
+++ lab-1/assignment_one.c
@@ -69,70 +69,18 @@ void test_print_char_implementation(void (*functionPtr)(void), int lines_to_test
 
 
 void test(void){
-
-    // // fetch expected output and save in an array
-    // const char *expected_output_data_path = "assignment_one_expected_output_test_data";
-    // FILE *expected_data_file = fopen(expected_output_data_path, "r");
-    // if (expected_data_file == NULL) {
-    //     printf("Could not open file '%s'", expected_output_data_path);
-    //     // return ERROR_NUMBER
-    // }
-    // // allocate string array that holds 4 strings
-    // char **expected_output = (char**) malloc(sizeof(char*)*4);
-    // int BUFFER_SIZE = 20;
-    // for(int i = 0; i < 4; ++i){
-    //     char *output = (char*)malloc(sizeof(char)*BUFFER_SIZE);
-    //     fgets(output, BUFFER_SIZE, expected_data_file);
-    //     *(expected_output+i) = output;
-    // }
-    // printf("Print array\n");
-    // char **ptr = NULL;
-    // for(ptr = expected_output; ptr<expected_output+4; ++ptr){
-    //     printf("%s", *ptr);
-    // }
-
-    // // set stdinput stream to input from file
-    // const char *input_data_path = "assignment_one_input_test_data";
-    // freopen(input_data_path, "r", stdin);
-
-    // print_reversed_input_rec();
-    // print_reversed_input_rec();
-    // print_reversed_input_rec();
-    // print_reversed_input_rec();
-
-    // Usage of test: start program with
-
-    
-    // for(int i = 0; i < 3; ++i){
-    //     printf("Actual output:   ");
-    //     print_reversed_input_iter();
-    //     printf("\n");
-    //     char input[20];
-    //     fgets(input, 20, stdin);
-    //     printf("Expected output: %s", input);
-    //     printf("\n");
-    // }
     printf("Testing iterative reversed input:\n");
-    test_print_char_implementation(print_reversed_input_iter, 3);
+    test_print_char_implementation(&print_reversed_input_iter, 3);
     printf("Testing recursive reversed input:\n");
-    test_print_char_implementation(print_reversed_input_rec, 3);
+    test_print_char_implementation(&print_reversed_input_rec, 3);
 
 }
 
+void options(char** argv){
+    // check if flag -T in args else run normal operation
+}
 
-int main(void){
-    // print_user_prompt();
-    // print_reversed_input_rec();
-
-    // print_user_prompt();
-    // print_reversed_input_iter();
-
-    // test();
-
-    // fputs("12345678901234567890", stdin);
-    // char input[20];
-    // fgets(input, 20, stdin);
-    // printf("%s", input);
+int main(char** argv){
 
     test();
 
