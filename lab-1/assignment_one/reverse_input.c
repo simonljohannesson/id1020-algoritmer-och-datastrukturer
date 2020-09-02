6285da1dabd3d722da242a7b355cc3858e792df8
Simon Johannesson <simonljohannesson@gmail.com>
2020-09-02 19:22:54 +0200

    Lab 1, Assignment 1: added test handling.

--- lab-1/assignment_one/reverse_input.c
+++ lab-1/assignment_one/reverse_input.c
@@ -2,13 +2,18 @@
  *  Author:         Simon Johannesson
  *  Email:          simonljohannesson@gmail.com, sijohann@kth.se
  *  Created:        2020-08-27
- *  Updated: 
- *  Solves problem: 
- *  Usage:          
+ *  Updated:        2020-09-02
+ *  Solves problem: Lab 1, assignment 1.
+ *  Usage:          For normal operation, run program without arguments and follow the prompts.
+ *                  For testing operation, run program with -T as the only argument and pipe the
+ *                  contents of 'test_data' as the standard input of the program. For example:
+ *                  In bash, run the command './program -T < test_data', if program is the name
+ *                  of the binary that resulted from compiling this file.
  *  Based on:       None.
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* 
     takes input from stdin until newline character is received
@@ -67,8 +72,7 @@ void test_print_char_implementation(void (*functionPtr)(void), int lines_to_test
     }
 }
 
-
-void test(void){
+void run_test(void){
     printf("Testing iterative reversed input:\n");
     test_print_char_implementation(&print_reversed_input_iter, 3);
     printf("Testing recursive reversed input:\n");
@@ -76,19 +80,33 @@ void test(void){
 
 }
 
-int test_flag_set(char* argc, char** argv){
-    // check if flag -T in args else run normal operation
-    if (*argc > 1){
-        int i = 0;
-        while (i != 0x00){
-            
-        }
-    }
+/*
+    check if flag '-T' in args
+
+    return 1 if flag is first argument in args, else return 0
+*/
+int test_flag_set(int argc, char** argv){
+    
+    const char* const test_flag = "-T";
+    if (argc > 1  &&  0 == strcmp(test_flag, argv[1])) return 1;
+    else return 0;
 }
 
-int main(char* argc, char** argv){
+int main(int argc, char** argv){
 
-    test();
+    if (test_flag_set(argc, argv)){
+        run_test();
+    }
+    else {
+        printf("Iterative: ");
+        print_user_prompt();
+        print_reversed_input_iter();
+
+        printf("Recursive: ");
+        print_user_prompt();
+        print_reversed_input_rec();
+    }
+
 
     return 0;
 }
