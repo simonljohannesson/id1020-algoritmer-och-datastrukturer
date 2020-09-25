9a2c0010cc8b262206fb950928ae16eb8a653a35
Simon Johannesson <simonljohannesson@gmail.com>
2020-09-25 22:50:34 +0200

    Lab 3, implemented binary search st and bst

---
+++ lab-3/java-impl/src/BST.java
@@ -0,0 +1,139 @@
+import java.util.NoSuchElementException;
+
+public class BST<Key extends Comparable<Key>, Value>{
+    private Node root;
+    private class Node{
+        private Key key;
+        private Value val;
+        private Node left, right;
+        private int size;           // nodes in subtrees
+
+        public Node(Key key, Value val, int size){
+            this.key = key;
+            this.val = val;
+            this.size = size;
+        }
+    }
+    public BST(){}
+    public int size(){
+        return size(root);
+    }
+    private int size(Node x){
+        if(x==null) return 0;
+        else return x.size;
+    }
+    public boolean isEmpty(){
+        return size() == 0;
+    }
+    private Value get(Node x, Key key){
+        if (key == null) throw new IllegalArgumentException("Argument 'key' cannot be null.");
+        if (x == null) return null;  // at bottom of tree, key not in symbol table
+        int cmp = key.compareTo(x.key);
+        if      (cmp < 0) return get(x.left, key);
+        else if (cmp > 0) return get(x.right, key);
+        else              return x.val;
+    }
+    public Value get(Key key){
+        return get(root, key);
+    }
+    public boolean contains(Key key){
+        if (key == null) throw new IllegalArgumentException("Argument 'key' cannot be null.");
+        return get(key) != null;
+    }
+    private Node put(Node x, Key key, Value val){
+        if(x == null) return new Node(key, val, 1); // bottom of tree
+        int cmp = key.compareTo(x.key);
+        if      (cmp < 0) x.left = put(x.left, key, val);
+        else if (cmp > 0) x.right = put(x.right, key, val);
+        else              x.val = val;                      // overwrite old value
+        // on the way back up the stack update sizes
+        x.size = 1+ size(x.left) + size(x.right);
+        return x;
+    }
+    public void put(Key key, Value val){
+        if (key == null) throw new IllegalArgumentException("Argument 'key' cannot be null.");
+        if (val == null) {
+            delete(key);
+            return;
+        }
+        root = put(root, key, val);
+    }
+    private Node min(Node x){
+        if(x.left == null) return x; // no more smaller keys
+        else return min(x.left);
+    }
+    private Node max(Node x){
+        if(x.right == null) return x; // no more bigger keys
+        else return max(x.right);
+    }
+    public Key min(){
+        if(isEmpty()) throw new NoSuchElementException("Symbol table empty.");
+        return min(root).key;
+    }
+    public Key max(){
+        if(isEmpty()) throw new NoSuchElementException("Symbol table empty.");
+        return max(root).key;
+    }
+    private Node deleteMin(Node x){
+        if(x.left == null) return x.right; // 'cut out' x from the list at the bottom of tree
+        x.left = deleteMin(x.left); // keep following the left fork
+        // update sizes on the way back on the stack
+        x.size = 1+ size(x.left) + size(x.right);
+        return x;
+    }
+    public void deleteMin(){
+        if(isEmpty()) throw new NoSuchElementException("Symbol table empty.");
+        root = deleteMin(root);
+    }
+    private Node deleteMax(Node x){
+        if(x.right == null) return x.left; // 'cut out' x from the list at the bottom of tree
+        x.right = deleteMax(x.right);     // follow the right fork
+        x.size = 1 + size(x.left) + size(x.right);
+        return x;
+    }
+    public void deleteMax(){
+        if(isEmpty()) throw new NoSuchElementException("Symbol table empty.");
+        root = deleteMax(root);
+    }
+    private Node delete(Node x, Key key){
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if      (cmp < 0)  x.left = delete(x.left, key);
+        else if (cmp > 0)  x.right = delete(x.right, key);
+        else { // key to delete found
+            // if there is only one branch just return that branch
+            if(x.right == null) return x.left;
+            if(x.left == null)  return x.right;
+            // comment, could just as well go left and take max(temp.left)
+            Node temp = x;
+            // this is where the old x node is 'written over' with the nearest value bigger than itself
+            x = min(temp.right);
+            x.right = deleteMin(temp.right);
+            x.left = temp.left;
+        }
+        // update size
+        x.size = 1 + size(x.left) + size(x.right);
+        return x;
+    }
+    public void delete(Key key){
+        if (key == null) throw new IllegalArgumentException("Argument 'key' cannot be null.");
+        root = delete(root, key);
+    }
+    public static void main(String[] args){
+
+        BST<String, Integer> bst = new BST<>();
+        for(int i = 0; i < 5000; ++i){
+            String s = Integer.toString(i);
+            bst.put(s, i*2);
+        }
+        System.out.println(bst.size());
+        for(int i = 400; i < 900; ++i){
+            bst.delete(Integer.toString(i));
+        }
+        System.out.println(bst.size());
+
+        for(int i = 2500; i < 2550; ++i){
+            System.out.println(bst.get(Integer.toString(i)));
+        }
+    }
+}
---
+++ lab-3/java-impl/src/BinarySearchST.java
@@ -0,0 +1,124 @@
+public class BinarySearchST <Key extends Comparable<Key>, Value>{
+    private static final int INIT_CAPACITY = 10;
+    private Key[] keys;
+    private Value[] vals;
+    private int n = 0;
+
+    /**
+     * Constructor, initialize empty symbol table
+     */
+    public BinarySearchST(){
+        this(INIT_CAPACITY);
+    }
+    public BinarySearchST(int capacity){
+        keys = (Key[]) new Comparable[capacity];
+        vals = (Value[]) new Object[capacity];
+    }
+    private void resize(int capacity){
+        //debug
+        if(n > capacity){
+            System.out.println("resizing to smaller array than what is allowed");
+        }
+        // create new lists that will replace the old ones
+        Key[] new_keys = (Key[]) new Comparable[capacity];
+        Value[] new_vals = (Value[]) new Object[capacity];
+        // copy all the data
+        for (int i = 0; i<n; ++i){
+            new_keys[i] = keys[i];
+            new_vals[i] = vals[i];
+        }
+        keys = new_keys;
+        vals = new_vals;
+    }
+    public int size(){
+        return n;
+    }
+    public boolean isEmpty(){
+        return size() == 0;
+    }
+    public int rank(Key key){
+        if (key == null) throw new IllegalArgumentException("Argument 'key' cannot be 'null'.");
+        // do binary search for the position of key
+        int lo = 0, hi = n-1;
+        while (lo <= hi){
+            int mid = lo + (hi - lo) / 2;
+            // if 0:  key == mid
+            // if -1: key < mid
+            // if 1:  key > mid
+            int cmp = key.compareTo(keys[mid]);
+            if      (cmp < 0) hi = mid-1;
+            else if (cmp > 0) lo = mid+1;
+            else return mid;
+        }
+        // if key is less than the smallest lo will not have moved from index 0
+        // if key is bigger than the biggest lo will end up on index n (and while loop will exit)
+        // or keys index in list if it had been in the list
+        return lo;
+    }
+    public Value get(Key key){
+        if (key == null) throw new IllegalArgumentException("Argument 'key' cannot be null'.");
+        if (isEmpty()) return null;
+        int i = rank(key);
+        if(i < n && keys[i].compareTo(key) == 0) return vals[i];
+        return null;
+    }
+    public void delete(Key key){
+        if (key == null) throw new IllegalArgumentException("Argument 'key' cannot be null'.");
+        if (isEmpty())return;
+        int i = rank(key);
+        // i bigger than biggest element  ||  i smaller than smallest element
+        // i.e. key not in table
+        if(i == n  ||  keys[i].compareTo(key) != 0) return;
+        // move all elements after the key one step 'left'
+        for (int j = i; j < n-1; ++j){
+            keys[j] = keys[j+1];
+            vals[j] = vals[j+1];
+        }
+        --n;
+        keys[n] = null;
+        vals[n] = null;
+        // resize if 1/4 full
+        if (n > 0  &&  n == keys.length/4) resize(keys.length/2);
+    }
+    public void put(Key key, Value value){
+        if (key == null) throw new IllegalArgumentException("Argument 'key' cannot be null'.");
+        if (value == null){
+            delete(key);
+            return;
+        }
+        int i = rank(key);
+        // if key in table replace it's value
+        if (i < n  &&  keys[i].compareTo(key) == 0){
+            vals[i] = value;
+            return;
+        }
+        // insert the new key value pair
+        if (n == keys.length) resize(2*keys.length);
+        for(int j = n; j > i; --j){
+            keys[j] = keys[j-1];
+            vals[j] = vals[j-1];
+        }
+        keys[i] = key;
+        vals[i] = value;
+        ++n;
+    }
+    public boolean isSorted(){
+        for (int i = 0; i < size()-1; ++i){
+            if (keys[i].compareTo(keys[i+1]) > 0) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        BinarySearchST<String, Integer> bsst = new BinarySearchST<>();
+
+
+        for (int i = 0; i < 500; i++){
+            String s = Integer.toString(i);
+            bsst.put(s, i*2);
+        }
+        System.out.println(bsst.get("456"));
+        System.out.println(bsst.get("5000"));
+
+
+    }
+}
